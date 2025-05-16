26da1dc8a4186bb551a3e509834b6d5a625aa66b
AnBLarsen <andrealarsen@protonmail.com>
2025-05-15 22:38:24 -0400

    Restore accidentally deleted files and add untracked folders

---
+++ src/app/[dashboard]/store/page.tsx
@@ -0,0 +1,11 @@
+
+import EditableBanner from '@/components/adminComponents/editableRestaurant/landing page/EditableBanner';
+
+export default function AdminPreviewPage() {
+  return (
+    <div className="mx-auto">
+      <EditableBanner />
+      
+    </div>
+  );
+}
