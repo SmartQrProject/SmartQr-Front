acc39fe29e26c2af4126b2913a4c226499c0d667
AnBLarsen <andrealarsen@protonmail.com>
2025-05-19 12:56:04 -0400

    creted reports and settings page and component files

---
+++ src/app/reports/page.tsx
@@ -0,0 +1,14 @@
+import Reports from '@/components/adminComponents/reports/Reports'
+import React from 'react'
+
+const ReportPage = () => {
+  return (
+    <>
+    <div>ReportPage</div>
+    <Reports/>
+    
+    </>
+  )
+}
+
+export default ReportPage
