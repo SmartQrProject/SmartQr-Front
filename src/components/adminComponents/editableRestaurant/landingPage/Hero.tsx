999d4c27ab7dfaeccdd8bd691afc2624855ef33d
AnBLarsen <andrealarsen@protonmail.com>
2025-05-20 15:19:04 -0400

    Moved the images of the landing page to cloudinary. Added frames motion to the hero. Please, do npm i

--- src/components/adminComponents/editableRestaurant/landingPage/Hero.tsx
+++ src/components/adminComponents/editableRestaurant/landingPage/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero({ name }: HeroProps) {
     <div className="relative w-full h-64 md:h-150 bg-cover bg-center flex items-center justify-center">
       <div className="absolute inset-0 bg-black/50"></div>
       <h1 className="relative text-white text-4xl md:text-5xl font-bold z-10">
-        {name}
+        Welcome to {name}
       </h1>
     </div>
   );
