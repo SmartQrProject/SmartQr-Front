16cc3240d43dc5f876d47c5bf5bc10a23b940c07
elitangarife <elianatangarifeblandon@gmail.com>
2025-05-22 14:07:23 -0500

    upd-2

--- src/app/customer/dashboard/page.tsx
+++ src/app/customer/dashboard/page.tsx
@@ -1,12 +1,12 @@
-import ListProducts from '@/components/adminComponents/menu/views/ListProductsView'
-import NavbarCustomer from '@/components/customers/navbarCustomer/NavbarCustomer'
+import ListProducts from '@/components/adminComponents/menu/views/ProductsView'
+
 import CustomerProfile from '@/components/customers/Profile'
 import React from 'react'
 
 const CustomerProfilePage = () => {
   return (
     <>
-    <NavbarCustomer/>
+ 
     
         <CustomerProfile/>
         <ListProducts/>
