5c5745a9097bc7ee681aa1494f319c9ad36b6b71
AnBLarsen <andrealarsen@protonmail.com>
2025-05-22 20:59:54 -0400

    pages for gabriel

---
+++ src/app/benefits/page.tsx
@@ -0,0 +1,15 @@
+import Benefits from '@/components/gabriel/Benefits';
+import React from 'react'
+
+const benefitsPage = () => {
+  return (
+
+    <> 
+      <div>Benefits</div>
+      <Benefits/>
+    
+    </>
+  )
+}
+
+export default benefitsPage;
---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,14 @@
+import Pricing from '@/components/gabriel/Pricing';
+import React from 'react'
+
+const PricingPage = () => {
+  return (
+    <>
+        <div>PricingPage</div>
+        <Pricing/>
+
+    </>
+  )
+}
+
+export default PricingPage;
