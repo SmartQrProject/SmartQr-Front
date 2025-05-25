be19451da17e59c0610249c621ce5385e66a7fa7
AnBLarsen <andrealarsen@protonmail.com>
2025-05-25 06:36:54 -0400

    added logo and favicon

--- src/components/subscribers/navbar/Navbar.tsx
+++ src/components/subscribers/navbar/Navbar.tsx
@@ -3,36 +3,36 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 
 const Navbar = () => {
+    const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_NAME;
+    const [open, setOpen] = useState(false)
 
-  const [open, setOpen] = useState(false)
+    return (
+        
+        <div >
+            <nav className='m-5 '>
 
-  return (
-    
-    <div >
-        <nav className='m-5 '>
+                <div className='flex justify-between items-center '>
+                    <Link className='text-[#6494ED] font-bold text-xl ' href={'/'}><><img src={`https://res.cloudinary.com/${cloudName}/image/upload/logo2_jzvw9b.png`} alt="SmartQR" className='h-16 inline-block rounded-lg' /></></Link>
 
-            <div className='flex justify-between items-center '>
-                <Link className='text-[#6494ED] font-bold text-xl ' href={'/'}>SmartQR</Link>
-
-                <button className='sm:hidden text-2xl ' onClick={() => setOpen(!open)}>☰</button>
-                <div className='hidden sm:flex space-x-4 items-center '>
-                    <Link href={'/features'}>Features</Link>
-                    <Link href={'/benefits'}>Benefits</Link>
-                    <Link href={'/pricing'}>Pricing</Link>
-                    <Link className='py-2 px-6 bg-[#6494ED] text-white rounded-3xl ' href={'/login'}>Sign In</Link>
+                    <button className='sm:hidden text-2xl ' onClick={() => setOpen(!open)}>☰</button>
+                    <div className='hidden sm:flex space-x-4 items-center '>
+                        <Link href={'/features'}>Features</Link>
+                        <Link href={'/benefits'}>Benefits</Link>
+                        <Link href={'/pricing'}>Pricing</Link>
+                        <Link className='py-2 px-6 bg-[#6494ED] text-white rounded-3xl ' href={'/login'}>Sign In</Link>
+                    </div>
                 </div>
-            </div>
-            
-            {open && (
-                <div className='flex flex-col space-y-2 mt-4 sm:hidden '>
-                    <Link href={'/features'}>Features</Link>
-                    <Link href={'/benefits'}>Benefits</Link>
-                    <Link href={'/pricing'}>Pricing</Link>
-                    <Link className='py-2 px-6 bg-[#6494ED] text-white rounded-3xl w-fit ' href={'/login'}>Sign In</Link>
-                </div>
-            )}
-        </nav>
-    </div>
+                
+                {open && (
+                    <div className='flex flex-col space-y-2 mt-4 sm:hidden '>
+                        <Link href={'/features'}>Features</Link>
+                        <Link href={'/benefits'}>Benefits</Link>
+                        <Link href={'/pricing'}>Pricing</Link>
+                        <Link className='py-2 px-6 bg-[#6494ED] text-white rounded-3xl w-fit ' href={'/login'}>Sign In</Link>
+                    </div>
+                )}
+            </nav>
+        </div>
   )
 }
 
