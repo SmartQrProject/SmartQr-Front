447f1f8a9bcd9839c32454686e4a1cd44337bae0
elitangarife <elianatangarifeblandon@gmail.com>
2025-05-29 22:59:03 -0500

    estaticaLogoutCustomer

--- src/components/auth0/AuthLinks.tsx
+++ src/components/auth0/AuthLinks.tsx
@@ -25,7 +25,7 @@ export default function AuthLinks() {
                 </a>
             ) : (
                 <a
-                    href="/auth/logout"
+                    href="/customer/dashboard/logout"
                     onClick={handleLogout}
                     className="block bg-default-700 text-white font-semibold px-4 py-2 rounded-md hover:text-default-600 transition w-full text-center"
                 >
