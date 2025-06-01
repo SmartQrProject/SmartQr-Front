c4009b20c552673bb54e99df8a287718f1f77df8
MatiasGGallardo <matiasgallardo196@gmail.com>
2025-06-01 14:34:10 +1000

    Refactor user profile settings: sanitize input data and enhance validation schema

--- src/components/adminComponents/settings/SettingsSchema.ts
+++ src/components/adminComponents/settings/SettingsSchema.ts
@@ -1,35 +1,47 @@
 import { z } from "zod";
 
-export const UserProfileSchema = z.object({
-  name: z
-    .string()
-    .min(5, "Name must be at least 5 characters")
-    .max(100, "Name must be at most 100 characters")
-    .regex(/^[A-Za-z0-9 ]+$/, {
-      message: "Name can only contain letters, numbers, and spaces",
-    }),
-  phone: z
-    .string()
-    .regex(/^\+?[1-9]\d{1,14}$/, "Please enter a valid phone number")
-    .optional(),
+export const UserProfileSchema = z
+  .object({
+    name: z
+      .string()
+      .min(5, "Name must be at least 5 characters")
+      .max(100, "Name must be at most 100 characters")
+      .regex(/^[A-Za-z0-9 ]+$/, {
+        message: "Name can only contain letters, numbers, and spaces",
+      })
+      .optional()
+      .or(z.literal("")), // permite string vacío
 
-  password: z.string().optional(),
-  confirmPassword: z.string().optional(),
-}).refine((data) => {
-  
-  if (data.password && !data.confirmPassword) {
-    return false;
-  }
-  if (data.confirmPassword && !data.password) {
-    return false;
-  }
-  if (data.password && data.password !== data.confirmPassword) {
-    return false;
-  }
-  return true;
-}, {
-  message: "Passwords must match",
-  path: ["confirmPassword"]
-});
+    phone: z
+      .string()
+      .regex(/^\+?[1-9]\d{1,14}$/, "Please enter a valid phone number")
+      .optional()
+      .or(z.literal("")),
 
-export type UserProfileFormInputs = z.infer<typeof UserProfileSchema>; 
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .max(15, "Password must be at most 15 characters")
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/, {
+        message:
+          "Password must include uppercase, lowercase, number, and special character",
+      })
+      .optional()
+      .or(z.literal("")),
+
+    confirmPassword: z.string().optional().or(z.literal("")),
+  })
+  .refine(
+    (data) => {
+      if (data.password || data.confirmPassword) {
+        return data.password === data.confirmPassword;
+      }
+      return true;
+    },
+    {
+      message: "Passwords must match",
+      path: ["confirmPassword"],
+    }
+  );
+
+export type UserProfileFormInputs = z.infer<typeof UserProfileSchema>;
